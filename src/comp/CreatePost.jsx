a8d8bf6ccd9ce58b24c6536d4894b22c7a84c113
ARJ-U-N <ghub70242@gmail.com>
2025-04-19 12:00:51 +0530

    post

--- src/comp/CreatePost.jsx
+++ src/comp/CreatePost.jsx
@@ -1,6 +1,45 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const CreatePost = () => {
+    const [token, setoken] = useState(sessionStorage.getItem("token"))
+
+
+
+
+
+    const [input, setinput] = useState(
+        			
+			
+		{"userId":sessionStorage.getItem("userId"),"Message":""}
+		
+
+
+
+    )
+    const ih = (e) => {
+        setinput({ ...input, [e.target.name]: e.target.value })
+      }
+      const rv=()=>{
+        console.log(input)
+        axios.post("http://localhost:3030/create",input,{
+            headers:{"token":token,"Content_Type":"application/json"}
+        }).then(
+            (response)=>{
+console.log(response.data)
+
+                if (response.data.status==="Success ") {
+                    alert("posted succesfully")
+                } else {
+                    alert("error")
+                }
+            }
+        ).catch(
+            (error)=>{
+                console.log(error)
+            }
+        )
+      }
   return (
     <div>
          <div className="container">
@@ -9,10 +48,10 @@ const CreatePost = () => {
                 <div className="row g-3">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <label htmlFor="" className="form-label">post a message</label>
-                        <textarea name="" id="" className="form-control"></textarea>
+                        <textarea name="Message" value={input.Message} className="form-control" onChange={ih}></textarea>
                     </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <button className="btn btn-success">post</button>
+                        <button className="btn btn-success" onClick={rv}>post</button>
                     </div>
                 </div>
                 </div>
